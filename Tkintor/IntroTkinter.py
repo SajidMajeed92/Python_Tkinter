2fa240cd0bfc14ab61af1ba2f7074b9bca5aad25
sajid <sajidmajeed.ist@gmail.com>
2022-12-22 21:08:12 +0500

    Creating Tkinter Introduction File

---
+++ Tkintor/IntroTkinter.py
@@ -0,0 +1,15 @@
+# Tkinter Programming
+# Tkinter is the standard GUI library for Python.
+# Python when combined with Tkinter provides a fast and easy way to create GUI applications.
+# Tkinter provides a powerful object-oriented interface to the Tk GUI toolkit.
+
+# The foundational element of a Tkinter GUI is the window.
+# Windows are the containers in which all other GUI elements live.
+# These other GUI elements, such as text boxes, labels, and buttons, are known as widgets.
+# Widgets are contained inside of windows.
+
+
+import tkinter as tk
+window = tk.Tk()
+'''Any Widgets you wants are added here '''
+window.mainloop()
